e51727b2dc135c80402edd60099f9309dc6707ba
YANG Yizhi <yizhi.yang@ens.uvsq.fr>
2024-04-24 22:32:43 +0200

    hehehe

--- LAB_02_03-SHARED_MEMORY/exercises/cross-stencil/include/mdspan_type_util.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "mdspan.hpp"
-
-#include <cstddef>
-
-template <typename T>
-using mdspan_2d =
-    Kokkos::mdspan<T, Kokkos::extents<size_t, Kokkos::dynamic_extent, Kokkos::dynamic_extent>>;
--- LAB_02_03-SHARED_MEMORY/exercises/integer-sum-redux/include/sequential.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <cstdint>
-#include <vector>
-
-namespace sum_reduce {
-/// Sequential 32-bit integer sum reduction.
-auto sequential(std::vector<int32_t> const& vec) -> int64_t;
-} // namespace sum_reduce
